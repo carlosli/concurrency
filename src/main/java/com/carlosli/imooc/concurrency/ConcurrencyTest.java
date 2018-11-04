25a4fe24033a24d42902f1455bb17f45dba75e1f
carlosli <carlosli>
2018-11-05 07:49:14 +0800

    atomic

---
+++ src/main/java/com/carlosli/imooc/concurrency/ConcurrencyTest.java
@@ -0,0 +1,47 @@
+package com.carlosli.imooc.concurrency;
+
+import com.carlosli.imooc.concurrency.controller.TestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+public class ConcurrencyTest {
+
+    protected static Logger logger = LoggerFactory.getLogger(TestController.class);
+
+
+    public static int clientTotal = 5000;
+
+    public static int threadTotal = 200;
+
+    public static int count = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        Semaphore semaphore = new Semaphore(threadTotal);
+        CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        for (int i = 0; i < clientTotal; i++) {
+            executorService.submit(() -> {
+                try {
+                    semaphore.acquire();
+                    add();
+                    semaphore.release();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                countDownLatch.countDown();
+            });
+        }
+
+        countDownLatch.await();
+        logger.info("count:{}", count);
+    }
+
+    private static void add() {
+        count++;
+    }
+}
