916f2aaaa588802b60fba5303f547ce0b2033e52
carlosli <carlosli>
2018-11-04 13:23:31 +0800

    add annotation

---
+++ src/main/java/com/carlosli/imooc/concurrency/annotation/ThreadSafe.java
@@ -0,0 +1,14 @@
+package com.carlosli.imooc.concurrency.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 用来标记线程 安全 的类
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface ThreadSafe {
+}
